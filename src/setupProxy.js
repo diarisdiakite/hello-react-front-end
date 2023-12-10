159d03c295b7f57b2b58f980768e55fc0a2570bb
diarisdiakite <diarisdiakite@gmail.com>
2023-12-10 18:15:29 +0100

    Run linters.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
 // src/setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function setupProxy(app) {
   app.use(
     '/messages',
     createProxyMiddleware({
       target: 'http://localhost:3000',
       changeOrigin: true,
-    })
+    }),
   );
 };
